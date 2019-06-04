a5183e28c09f6b64903c15549eccb57969fd72e1
chrisbai <2249355314@qq.com>
2019-06-04 19:15:23 +0800

    practice

--- test.c
+++ test.c
@@ -3,25 +3,6 @@
 
 int main()
 {
-	char *J = "aA";
-	char *S = "aAAbbbb";
-	int counter=0;
-	int i, j;
-   
-	printf("sizeof J(%d) S(%d)\n", strlen(J), strlen(S));
- 
-	for(i=0; i<strlen(J); i++)
-	{
-		for(j=0; j<strlen(S); j++)
-		{
-			if( strncmp(&J[i], &S[j], 1)==0)
-			{
-				printf("i(%d) j(%d) J[%d](%c) S[%d](%c)\n ", i, j, i, J[i], j, S[j]);
-				counter+=1;
-			}
-        	}
-	}
-    printf("counter %d\n", counter);
-
+	printf("bhc's pull");
 	return 0;
 }
